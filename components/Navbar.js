2d06a63095ebe9ca257d4fc1ba99093eceb0e2c8
Marcellus Nwankwo <cellusnwankwo@gmail.com>
2022-05-28 09:48:20 +0100

    identity Added

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,7 +1,7 @@
 import Link from 'next/link'
 import Image from 'next/image'
 import { useContext } from 'react'
-import AuthContext from '../stores/authContent'
+import AuthContext from '../stores/authContext'
 
 export default function Navbar() {
   const { user, login, logout, authReady } = useContext(AuthContext)
--- pages/guides.js
+++ pages/guides.js
@@ -1,26 +1,58 @@
-import { useEffect, useContext } from 'react'
-import AuthContext from '../stores/authContent'
+import { useContext, useEffect, useState } from 'react'
 import styles from '../styles/Guides.module.css'
+import AuthContext from '../stores/authContext'
 
 export default function Guides() {
   const { user, authReady } = useContext(AuthContext)
+  const [guides, setGuides] = useState(null)
+  const [error, setError] = useState(null)
 
-  // console.log(user.token.access_token)
-  useEffect(()=> {
-    if (authReady){
+  useEffect(() => {
+    if (authReady) {
+      
       fetch('/.netlify/functions/guides', user && {
         headers: {
-          Authorizaton: 'Bearer ' + user.token.access_token
+          Authorization:  'Bearer ' + user.token.access_token
         }
       })
-      .then(res => res.json())
-      .then(data => console.log(data))
+      .then(res => {
+        if(!res.ok){
+          throw Error('You must be logged in to view this content')
+        }
+        return res.json()
+      })
+      .then(data => {
+        setGuides(data)
+        setError(null)
+      })
+      .catch((err) => {
+        setError(err.message)
+        setGuides(null)
+      })
     }
-  }, [user, authReady])
+
+  },[user, authReady])
 
   return (
     <div className={styles.guides}>
-      <h2>All Guides</h2>
+      
+      {!authReady && <div> Loading... </div>}
+
+      {error && (
+        <div className={styles.error}>
+          <p>{ error }</p>
+        </div>
+      )}
+
+        {guides && guides.map(guide => (
+          <div key={ guide.title }>
+            <h3>{ guide.title }</h3>
+            <h4>Writen by { guide.author }</h4>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae totam ut cum voluptates autem, aperiam quibusdam explicabo asperiores iste eveniet et minima, harum molestias 
+              consectetur voluptas? Soluta ipsa tempore nihil.</p>
+          </div>
+        ))}
+
     </div> 
   )
 }
